29eaabcb35a8b0189c44c67baecde4dc003c92cf
justHeel <justink_jamanika@hotmail.com>
2020-10-09 16:05:57 +0200

    werkt niet

---
+++ my-ss-project/gatsby-config.js
@@ -0,0 +1,57 @@
+module.exports = {
+  siteMetadata: {
+    title: `Gatsby Silverstripe CMS Starter`,
+    description: `Build the frontend for your Silverstripe site in Gatsby.`,
+    author: `@_unclecheese_`,
+  },
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-typescript`,
+    `gatsby-plugin-tslint`,
+    `gatsby-plugin-sass`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `gatsby-starter-silverstripe`,
+        short_name: `starter`,
+        start_url: `/`,
+        background_color: `#005b94`,
+        theme_color: `#005b94`,
+        display: `minimal-ui`,
+        icon: `src/images/ss-logo.png`, // This path is relative to the root of the site.
+      },
+    },
+    {
+      resolve: `gatsby-source-silverstripe`,
+      options: {
+        host: `http://ss-graph`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-typescript`,
+    },
+    {
+      resolve: "gatsby-source-graphql",
+      options: {
+        // This type will contain remote schema Query type
+        typeName: "TestProduct",
+        // This is the field under which it's accessible
+        fieldName: "products",
+        // URL to query from
+        url: "http://ss-graph/graphql",
+      },
+    },
+    // this (optional) plugin enables Progressive Web App + Offline functionality
+    // To learn more, visit: https://gatsby.dev/offline
+    // `gatsby-plugin-offline`,
+  ],
+}
